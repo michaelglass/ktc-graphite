e5c5bbe3113bae2308f8e4c9d2c091e63653a64b
Robert <taeil.choi@kt.com>
2013-12-17 18:55:45 +0900

    divide into master and peer recipe to support federated storage

--- recipes/master.rb
+++ recipes/master.rb
@@ -20,13 +20,23 @@
 include_recipe "services"
 include_recipe "ktc-utils"
 
+# Search graphite peer nodes
+query = "recipes:#{node['graphite']['server_recipe']} AND chef_environment:#{node.chef_environment}"
+search(:node, query) do |n|
+  # Update carbon-relay destinations
+  node.default['graphite']['carbon']['relay']['destinations'] << "#{n['ipaddress']}:2004"
+  # Update webapp cluster_servers
+  node.default['graphite']['web']['cluster_servers'] << "#{n['ipaddress']}:80"
+end
+
 include_recipe "graphite"
+include_recipe "graphite::carbon_relay"
 
+# Register main carbon-relay endpoint
 ::KTC::Network.node = node
-
 ip = ::KTC::Network.address "management"
 
 ep = Services::Endpoint.new "graphite",
   ip: ip,
-  port: node['graphite']['carbon']['line_receiver_port']
+  port: node['graphite']['carbon']['relay']['line_receiver_port']
 ep.save
