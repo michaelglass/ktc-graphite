593d4f6aacca8b74865e09c99fc32731d94c53bd
Jesse Nelson <spheromak@gmail.com>
2014-02-19 20:19:07 -0800

    update with upstream attrib locs

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,9 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_attribute "graphite"
-include_attribute "graphite::carbon_cache"
-include_attribute "graphite::carbon_relay"
+include_attribute 'graphite'
+include_attribute 'graphite::carbon_cache'
+include_attribute 'graphite::carbon_relay'
 
 default['graphite']['timezone'] = 'Asia/Seoul'
 default['graphite']['package_version'] = '0.9.10-1'
@@ -29,5 +29,5 @@ default['graphite']['carbon']['pickle_receiver_port'] = 2014
 
 default['graphite']['carbon']['relay']['line_receiver_port'] = 2003
 default['graphite']['carbon']['relay']['pickle_receiver_port'] = 2004
-default['graphite']['carbon']['relay']['method'] = "consistent-hashing"  # rules | consistent-hashing
-default['graphite']['carbon']['relay']['destinations'] = ["127.0.0.1:2014"]
+default['graphite']['carbon']['relay']['method'] = 'consistent-hashing'  # rules | consistent-hashing
+default['graphite']['carbon']['relay']['destinations'] = ['127.0.0.1:2014']
