d67d19e01a45c774413c0759810f2e53944f5f41
Wil Reichert <wil.reichert@kt.com>
2014-02-27 18:18:26 +0900

    rubocop update

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name              "ktc-graphite"
-maintainer        "Robert Choi"
-maintainer_email  "taeilchoi1@gmail.com"
-license           "Apache 2.0"
-description       "KTC wrapper for graphite cookbook"
+name              'ktc-graphite'
+maintainer        'Robert Choi'
+maintainer_email  'taeilchoi1@gmail.com'
+license           'Apache 2.0'
+description       'KTC wrapper for graphite cookbook'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.9.5'
 
-depends           "graphite"
-depends           "ktc-utils"
-depends           "services"
+depends           'graphite'
+depends           'ktc-utils'
+depends           'services'
