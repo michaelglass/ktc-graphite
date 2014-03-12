f85548ce241f90ea1950ae27cac585fed6a5dd27
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2014-03-13 05:20:21 +0900

    auto_version to 0.9.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'taeilchoi1@gmail.com'
 license           'Apache 2.0'
 description       'KTC wrapper for graphite cookbook'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.9.5'
+version '0.9.6'
 
 depends           'graphite'
 depends           'ktc-utils'
